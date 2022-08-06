fbaf1d912055de2cf328c1c3d84462470f3aff8d
Leonardo Nunes Martinha <leonardomartinha.dev@gmail.com>
2022-08-06 19:59:41 -0300

    Abrir menu

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -16,38 +16,44 @@ const Home = (data: Props) => {
   });
 
   const [products, setProducts] = useState<Product[]>(data.products);
+  const [openMenu, setOpenMenu] = useState(false);
 
   const handleSearch = (searchValue: string) => {
     console.log(searchValue)
   };
 
   return (
-    <div>
-      <header className="px-6 pt-[50px] pb-[30px] bg-[#F9F9FB]">
-        <div className="flex justify-between items-center ">
-          <div className="flex flex-col">
-            <span className="text-[24px] font-semibold text-[#1B1B1B] mb-1" >Seja bem vindo👋</span>
-            <span className="text-[15px] text-[#979797]" >O que deseja para hoje?</span>
-          </div>
-          <div className="flex flex-col w-[18px] h-[16px]" >
-            <div style={{ backgroundColor: tenant?.mainColor }} className='w-full h-[2px]'></div>
-            <div style={{ backgroundColor: tenant?.mainColor }} className='w-full h-[2px] my-1'></div>
-            <div style={{ backgroundColor: tenant?.mainColor }} className='w-full h-[2px]'></div>
-          </div>
-        </div>
-        <div className="w-full h-[60px] mt-6">
-          <SearchInput
-            onSearch={handleSearch}
-          />
-        </div>
-      </header>
-      <Banner />
-      <div className="m-auto grid grid-cols-2 px-6 gap-6">
-        {products.map((item, index) => (
-          <ProductItem data={item} key={index} />
-        ))}
+    <>
+      <div className={``}>
+
       </div>
-    </div>
+      <div className={`${openMenu ? "" : ""}`} > 
+        <header className="px-6 pt-[50px] pb-[30px] bg-[#F9F9FB]">
+          <div className="flex justify-between items-center ">
+            <div className="flex flex-col">
+              <span className="text-[24px] font-semibold text-[#1B1B1B] mb-1">Seja bem vindo👋</span>
+              <span className="text-[15px] text-[#979797]" >O que deseja para hoje?</span>
+            </div>
+            <div className="flex flex-col w-[18px] h-[16px]" onClick={() => setOpenMenu(!openMenu)} >
+              <div style={{ backgroundColor: tenant?.mainColor }} className='w-full h-[2px]'></div>
+              <div style={{ backgroundColor: tenant?.mainColor }} className='w-full h-[2px] my-1'></div>
+              <div style={{ backgroundColor: tenant?.mainColor }} className='w-full h-[2px]'></div>
+            </div>
+          </div>
+          <div className="w-full h-[60px] mt-6">
+            <SearchInput
+              onSearch={handleSearch}
+            />
+          </div>
+        </header>
+        <Banner />
+        <div className="m-auto grid grid-cols-2 px-6 gap-6">
+          {products.map((item, index) => (
+            <ProductItem data={item} key={index} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
