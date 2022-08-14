a7dfac7afd08b8f9d7c6717140605a713e134104
Leonardo Nunes Martinha <leonardomartinha.dev@gmail.com>
2022-08-14 11:56:17 -0300

    API de criação de usuário

--- libs/api.ts
+++ libs/api.ts
@@ -1,3 +1,6 @@
+import { error } from "console";
+import { NextApiResponse } from "next";
+
 export default {
   getUserByEmail: async (email: string) => {
     const user = await prisma?.user.findFirst({
@@ -9,5 +12,42 @@ export default {
     if(user) {
       return user;
     }
+  },
+
+  createNewUser: async (name: string, email: string, password: string, res: NextApiResponse) => {
+    if(name && email && password) {
+      const user = await prisma?.user.findFirst({ 
+        where: {
+          email
+        }
+       });
+
+       if(!user) {
+        if(password.length >= 6) {
+          const newUser = await prisma?.user.create({
+            data: {
+              name,
+              email,
+              password
+            }
+          });
+
+          if(newUser) {
+            return {
+              email: newUser.email,
+              password: newUser.password
+            };
+          } else {
+            res.status(400).json("Falha ao criar novo usuário")
+          };
+        } else {
+          res.status(400).json("Senha muito fraca")
+        };
+       } else {
+        res.status(400).json("Email já cadastrado")
+       };
+    } else {
+      res.status(400).json("Preencha todos os campos")
+    };
   }
 }
