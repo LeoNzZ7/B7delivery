5fefa8f52edeb0b08d6ac6fb300fea0f0470f534
Leonardo Nunes Martinha <leonardomartinha.dev@gmail.com>
2022-08-31 15:42:01 -0300

    Página da saola finalizada

--- pages/[tenant]/bag.tsx
+++ pages/[tenant]/bag.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { ArrowLeft } from "phosphor-react";
 import { useState } from "react";
+import { Button } from "../../components/button";
 import { Counter } from "../../components/counter";
 import { useApi } from "../../libs/useApi";
 import { Product } from "../../types/product";
@@ -11,11 +12,10 @@ import { Tenant } from "../../types/tenant";
 import { authOptions } from "../api/auth/[...nextauth]";
 
 const Bag = (data: Props) => {
-  const router = useRouter(); 
+  const router = useRouter();
 
   const [products, setProducts] = useState<Product[]>(data.products);
-
-  console.log(data.products);
+  const [cep, setCep] = useState(""); 
 
   return (
     <div className="px-6">
@@ -38,25 +38,83 @@ const Bag = (data: Props) => {
 
       {products &&
         products.map((item, index) => (
-          <div className="flex items-center justify-between h-[85px] w-[373px]" key={index} >
-            <div>
-              <img src={item.image} className="w-[85px] h-auto" />
+          <div>
+            <div className="flex items-center justify-between h-[85px] w-[373px]" key={index} >
+              <div>
+                <img src={item.image} className="w-[85px] h-auto" />
+              </div>
+              <div className="flex flex-col justify-between p-2" >
+                <span className="text-[12px] font-medium text-[#666]">{data.products[0].category}</span>
+                <span className="text-[#1B1B1B] text-[18px]">{data.products[0].name}</span>
+                <span className="text-[#FB9400] text-[16px] font-semibold" >{data.products[0].price}</span>
+              </div>
+              <div>
+                <Counter />
+              </div>
             </div>
-            <div className="flex flex-col justify-between p-2" >
-              <span className="text-[12px] font-medium text-[#666]">{data.products[0].category}</span>
-              <span className="text-[#1B1B1B] text-[18px]">{data.products[0].name}</span>
-              <span className="text-[#FB9400] text-[16px] font-semibold" >{data.products[0].price}</span>
-            </div>
-            <div>
-              <Counter />
-            </div>
+            <hr className="mb-4 mt-4" />
           </div>
         ))
-      }
-      <hr className="mb-4 mt-4" />
+      };
+      <div>
+        <div className="mb-5" > 
+          <span className="text-[#6A7D8B]" >Calcular frete e prazo</span>
+          <div className="mt-4 flex justify-between">
+            <input 
+              className="rounded-md h-[56px] w-[80%] border-2 focus:ring-0" 
+              style={{ borderColor: data.tenant.mainColor }} 
+              placeholder="12345-123" 
+              value={cep}
+              onChange={e => setCep(e.target.value)}
+              type="text"
+              />
+            <button className="uppercase border-2 font-semibold w-[56px] h-[56px] rounded-md" style={{ borderColor: data.tenant.mainColor, color: data.tenant.mainColor }} >Ok</button>
+          </div>
+        </div>
+        <div className="bg-[#F9F9FA] h-[94px] flex flex-col justify-center items-center rounded-sm my-5" >
+          <span className="text-[#6A7D8B] text-[10px] w-[85%]" >
+            Rua das Flores - Jardins da Serra - Campina Pequena
+          </span>
+          <div className="flex justify-between w-[85%] mt-2">
+            <span className="text-black font-normal" >
+              Receba em até 20 minutos
+            </span>
+            <span className="font-semibold" style={{ color: data.tenant.mainColor }} >
+              R$12,50
+            </span>
+          </div>
+        </div>
+        <div className="bg-[#F9F9FB] my-5 p-5">
+          <div className="flex justify-between">
+            <span>
+              Subtotal
+            </span>
+            <span>
+              R$102,00
+            </span>
+          </div>
+          <div className="flex justify-between my-2 border-opacity-60 border-b-2 border-dashed border-[#96A3AB] pb-3">
+            <span>
+              Frete
+            </span>
+            <span>
+              --
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span>
+              Total
+            </span>
+            <span className="text-[#FB9400] font-semibold text-[24px]" >
+              R$102,00
+            </span>
+          </div>
+          <Button invertColors={false} buttonText="Continuar" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Bag;
 
@@ -80,7 +138,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   if (!session) {
     return { redirect: { destination: '/singin', permanent: false } }
-  }
+  };
 
   const products = await api.getProductsBag(session.user.id.toString());
 
